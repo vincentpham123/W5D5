1591272187bf981d3ee2229730050378b5ee97d8
vincentpham123 <vincentpham32@gmail.com>
2023-04-28 17:05:53 -0700

    last pair push

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,18 +1,28 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+  Movie
+  .select(:id, :title, :yr, :score)
+  .where('yr BETWEEN 1980 AND 1989 AND score BETWEEN 3 AND 5')
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+  .select(:yr)
+  .group(:yr)
+  .having('MAX(score)<=8')
+  .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+  .select(:id, :name)
+  .joins(:movies)
+  .where(movies: {title: title}) # (table_name: {column_name:})
+  .order('ord ASC');
 end
 
 def vanity_projects
